0cba76a3db53c4e9bc290932a4a8c4f2386dbec0
Raphael da Silva <raphael_correa_silva@hotmail.com>
2020-04-26 18:02:18 -0300

    Adição de nova frase

--- var/quotes.php
+++ var/quotes.php
@@ -9,5 +9,9 @@ return [
 	[
 		"quote" => "Com grandes poderes vem grandes responsabilidades.",
 		"movie" => "Homem-aranha"
+	],
+	[
+		"quote" => "Você não pode ser o Homem-Aranha amigo da vizinhança se não existir vizinhança.",
+		"movie" => "Vingadores: guerra infinita"
 	]
 ];
